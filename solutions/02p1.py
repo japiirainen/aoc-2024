aa2c61112af02536be2823ede4da89f4114d1759
japiirainen <joona.piirainen@gmail.com>
2024-12-02 09:47:21 +0200

    refactor(02): simplify

--- solutions/02p1.py
+++ solutions/02p1.py
@@ -7,9 +7,6 @@ print(
             list(map(int, line.split())) for line in open(0).read().splitlines()
         )
         if (sorted(report) == report or sorted(report, reverse=True) == report)
-        and all(
-            abs(b - a) >= 1 and abs(b - a) <= 3
-            for a, b in zip(sorted(report), sorted(report)[1:])
-        )
+        and all(abs(b - a) >= 1 and abs(b - a) <= 3 for a, b in zip(report, report[1:]))
     )
 )
--- solutions/02p2.py
+++ solutions/02p2.py
@@ -10,8 +10,7 @@ for r in reports:
     for i in range(len(r)):
         report = r[:i] + r[i + 1 :]
         if (sorted(report) == report or sorted(report, reverse=True) == report) and all(
-            abs(b - a) >= 1 and abs(b - a) <= 3
-            for a, b in zip(sorted(report), sorted(report)[1:])
+            abs(b - a) >= 1 and abs(b - a) <= 3 for a, b in zip(report, report[1:])
         ):
             safe += 1
             break
