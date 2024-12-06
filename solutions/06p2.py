ce1f0dc5319cd5c4d271d544daa6c63027dfe512
japiirainen <joona.piirainen@gmail.com>
2024-12-06 13:28:14 +0200

    refactor(06): faster and simpler solutions

--- solutions/06p2.py
+++ solutions/06p2.py
@@ -1,47 +1,28 @@
 #!/usr/bin/env python
 
-grid = [list(line) for line in open(0).read().splitlines()]
+grid = {
+    complex(r, c): x
+    for r, line in enumerate(open(0))
+    for c, x in enumerate(line.strip())
+}
 
-(r, c) = next(
-    (r, c)
-    for r in range(len(grid))
-    for c in range(len(grid[r]))
-    if grid[r][c] in "><^v"
-)
-
-moves = {">": (0, 1), "v": (1, 0), "<": (0, -1), "^": (-1, 0)}
+start = next(c for c, x in grid.items() if x == "^")
 
 
-def next_dir(d: str) -> str:
-    keys = list(moves.keys())
-    return keys[(keys.index(d) + 1) % len(keys)]
-
-
-def loops(grid: list[list[str]], start: tuple[int, int]) -> bool:
-    r, c = start
-    dir = grid[r][c]
+def go(grid: dict[complex, str]) -> tuple[set[complex], bool]:
     seen = set()
-    while True:
-        seen.add((r, c, dir))
-        nr, nc = r + moves[dir][0], c + moves[dir][1]
-        if not (0 <= nr < len(grid) and 0 <= nc < len(grid[nr])):
-            return False
-        if grid[nr][nc] == "#":
-            dir = next_dir(dir)
+    pos, dir = start, -1
+
+    while pos in grid and (pos, dir) not in seen:
+        seen |= {(pos, dir)}
+        if grid.get(pos + dir) == "#":
+            dir *= -1j
         else:
-            r, c = nr, nc
-        if (r, c, dir) in seen:
-            return True
+            pos += dir
+
+    return ({pos for pos, _ in seen}, (pos, dir) in seen)
 
 
-count = 0
+path, *_ = go(grid)
 
-for i in range(len(grid)):
-    for j in range(len(grid[r])):
-        if grid[i][j] == ".":
-            grid[i][j] = "#"
-            if loops(grid, (r, c)):
-                count += 1
-            grid[i][j] = "."
-
-print(count)
+print(sum(loops for _, loops in (go(grid | {pos: "#"}) for pos in path)))
