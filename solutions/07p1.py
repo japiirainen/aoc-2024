927d6dcea8e5748b89c0414a84c5ea22f437f191
japiirainen <joona.piirainen@gmail.com>
2024-12-07 08:27:33 +0200

    refactor(07): simplify

--- solutions/07p1.py
+++ solutions/07p1.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
-
-from typing import Generator
-
+from itertools import cycle
 
 equations = [
     (int(line.split(": ")[0]), list(map(int, line.split(": ")[1].split())))
@@ -10,13 +8,12 @@ equations = [
 ]
 
 
-def go(nums: list[int]) -> Generator[int, None, None]:
+def go(nums: list[int]):
     if len(nums) == 1:
         yield nums[0]
         return
-    for res in go(nums[:-1]):
-        yield res * nums[-1]
-        yield res + nums[-1]
+    for res, x in zip(go(nums[1:]), cycle([nums[0]])):
+        yield from [res + x, res * x]
 
 
-print(sum(test for test, nums in equations if any(res == test for res in go(nums))))
+print(sum(t for t, n in equations if any(r == t for r in go(list(reversed(n))))))
