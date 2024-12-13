2463e5d9aba4a9fac7fb90751891f5fbb7dabef8
japiirainen <joona.piirainen@gmail.com>
2024-12-13 08:24:22 +0200

    feat(13): solve using numpy

--- solutions/13p2.py
+++ solutions/13p2.py
@@ -1,34 +1,15 @@
 #!/usr/bin/env python
 
 import re
-from collections import namedtuple
-
-configs = open(0).read().split("\n\n")
-
-Coord = namedtuple("Coord", ["x", "y"])
-Machine = namedtuple("Machine", ["a", "b", "goal"])
-
-machines: list[Machine] = []
-
-for config in configs:
-    a, b, g = config.splitlines()
-    a, b, g = [
-        list(map(int, x)) for x in map(lambda x: re.findall(r"\d+", x), [a, b, g])
-    ]
-    machines.append(
-        Machine(Coord(*a), Coord(*b), Coord(*[g + 10000000000000 for g in g]))
-    )
+import numpy as np
 
 
-def solve(machine: Machine) -> int:
-    a, b, p = machine
-    m = (p.x * b.y - p.y * b.x) // (a.x * b.y - a.y * b.x)
-    if m * (a.x * b.y - a.y * b.x) != (p.x * b.y - p.y * b.x):
-        return 0
-    n = (p.y - a.y * m) // b.y
-    if n * b.y != (p.y - a.y * m):
-        return 0
-    return 3 * m + n
+def solve(d):
+    ax, ay, bx, by, gx, gy = map(int, re.findall(r"\d+", d))
+    a = np.array([[ax, bx], [ay, by]])
+    b = np.array([gx, gy]) + 1e13
+    nx = np.linalg.solve(a, b)
+    return round(3 * nx[0] + nx[1]) if all(np.abs(np.round(nx) - nx) < 1e-3) else 0
 
 
-print(sum(map(solve, machines)))
+print(sum(map(solve, open(0).read().split("\n\n"))))
