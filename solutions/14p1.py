463fd929b6be0f823f014d6090fb270d40b73fc9
japiirainen <joona.piirainen@gmail.com>
2024-12-14 11:49:37 +0200

    feat(14): actual solution for part 2

--- solutions/14p1.py
+++ solutions/14p1.py
@@ -1,38 +1,32 @@
 #!/usr/bin/env python
 
 import re
-from collections import defaultdict
-from math import prod
+import math
 
 lines = open(0).read().splitlines()
 
 R = 103
 C = 101
 
-robots: dict[tuple[int, int], list[tuple[int, int]]] = defaultdict(list)
+robots = [tuple(map(int, re.findall(r"-?\d+", line))) for line in lines]
 
-for line in lines:
-    px, py, vx, vy = map(int, re.findall(r"-?\d+", line))
-    robots[(py, px)].append((vy, vx))
 
-for _ in range(100):
-    new_robots = defaultdict(list)
-    for (r, c), velocities in robots.items():
-        for vr, vc in velocities:
-            nr, nc = (r + vr) % R, (c + vc) % C
-            new_robots[(nr, nc)].append((vr, vc))
-    robots = new_robots
+def simulate(robot: tuple[int, ...]) -> tuple[int, ...]:
+    c, r, vc, vr = robot
+    return ((r + vr * 100) % R, (c + vc * 100) % C, vr, vc)
 
-scores = [0, 0, 0, 0]
 
-for (r, c), rs in robots.items():
-    if r < (R - 1) / 2 and c < (C - 1) / 2:
-        scores[0] += len(rs)
-    elif r < (R - 1) / 2 and c > (C - 1) / 2:
-        scores[1] += len(rs)
-    elif r > (R - 1) / 2 and c < (C - 1) / 2:
-        scores[2] += len(rs)
-    elif r > (R - 1) / 2 and c > (C - 1) / 2:
-        scores[3] += len(rs)
+quadrants = [0] * 4
 
-print(prod(scores))
+for r, c, _, _ in map(simulate, robots):
+    if r < (R - 1) // 2 and c < (C - 1) // 2:
+        quadrants[0] += 1
+    elif r < (R - 1) // 2 and c > (C - 1) // 2:
+        quadrants[1] += 1
+    elif r > (R - 1) // 2 and c < (C - 1) // 2:
+        quadrants[2] += 1
+    elif r > (R - 1) // 2 and c > (C - 1) // 2:
+        quadrants[3] += 1
+
+
+print(math.prod(quadrants))
--- solutions/14p2.py
+++ solutions/14p2.py
@@ -1,58 +1,38 @@
 #!/usr/bin/env python
 
 import re
-from collections import defaultdict
+import math
 
 lines = open(0).read().splitlines()
 
 R = 103
 C = 101
 
-robots: dict[tuple[int, int], list[tuple[int, int]]] = defaultdict(list)
-
-for line in lines:
-    px, py, vx, vy = map(int, re.findall(r"-?\d+", line))
-    robots[(py, px)].append((vy, vx))
+robots = [tuple(map(int, re.findall(r"-?\d+", line))) for line in lines]
 
 
-def image():
-    img = [["." for _ in range(C)] for _ in range(R)]
-    for r in range(R):
-        for c in range(C):
-            if (r, c) in robots:
-                img[r][c] = "#"
-            else:
-                img[r][c] = "."
-    return "\n".join("".join(row) for row in img)
+def safety_factor(second: int) -> int:
+    def simulate(robot: tuple[int, ...]) -> tuple[int, ...]:
+        c, r, vc, vr = robot
+        return ((r + vr * second) % R, (c + vc * second) % C, vr, vc)
+
+    quadrants = [0] * 4
+
+    for r, c, _, _ in map(simulate, robots):
+        if r < (R - 1) // 2 and c < (C - 1) // 2:
+            quadrants[0] += 1
+        elif r < (R - 1) // 2 and c > (C - 1) // 2:
+            quadrants[1] += 1
+        elif r > (R - 1) // 2 and c < (C - 1) // 2:
+            quadrants[2] += 1
+        elif r > (R - 1) // 2 and c > (C - 1) // 2:
+            quadrants[3] += 1
+
+    return math.prod(quadrants)
 
 
-def calc_togetherness(a):
-    t = 0
-    for i in range(len(a)):
-        for j in range(len(a[0])):
-            if a[i][j] != "#":
-                continue
-            for dx, dy in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
-                nx, ny = (i + dx, j + dy)
-                if 0 <= nx < len(a) and 0 <= ny < len(a[0]) and a[nx][ny] == "#":
-                    t += 1
-                    break
-    return t
-
-
-m = 0
-for i in range(10_000):
-    new_robots = defaultdict(list)
-    for (r, c), velocities in robots.items():
-        for vr, vc in velocities:
-            nr, nc = (r + vr) % R, (c + vc) % C
-            new_robots[(nr, nc)].append((vr, vc))
-    robots = new_robots
-
-    t = calc_togetherness(image())
-    m = max(m, t)
-
-    if t == m:
-        print("=" * 80)
-        print(image())
-        print(i)
+print(
+    min(
+        ((safety_factor(second), second) for second in range(R * C)), key=lambda x: x[0]
+    )[1]
+)
