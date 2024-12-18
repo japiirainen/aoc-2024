2e959e0e309e03b853198ebd98aa9fe90e83d3da
japiirainen <joona.piirainen@gmail.com>
2024-12-18 07:21:10 +0200

    feat(18): add solutions

---
+++ solutions/18p2.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from collections import deque
+
+R = 71
+C = 71
+
+bts = deque(
+    [tuple([int(x) for x in line.split(",")]) for line in open(0).read().splitlines()]
+)
+
+grid = [["." for _ in range(C)] for _ in range(R)]
+
+
+def way(grid):
+    r = c = 0
+
+    seen = {(r, c)}
+    q = deque([(r, c, 0)])
+
+    while q:
+        r, c, steps = q.popleft()
+
+        for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            nr, nc = r + dr, c + dc
+            if not (0 <= nr < R and 0 <= nc < C and (nr, nc)):
+                continue
+            if grid[nr][nc] == "#":
+                continue
+            if (nr, nc) in seen:
+                continue
+            if (nr, nc) == (R - 1, C - 1):
+                return steps + 1
+            seen.add((nr, nc))
+            q.append((nr, nc, steps + 1))
+    return None
+
+
+while bts:
+    br, bc = bts.popleft()
+    grid[br][bc] = "#"
+    if not way(grid):
+        print(f"{br},{bc}")
+        exit(0)
