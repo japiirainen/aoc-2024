f2350e495680401bc703f265e3b36382b192f226
japiirainen <joona.piirainen@gmail.com>
2024-12-21 07:56:12 +0200

    feat(20): simpler solutions

--- solutions/20p2.py
+++ solutions/20p2.py
@@ -1,55 +1,51 @@
 #!/usr/bin/env python
 
-import networkx as nx
+from collections import deque
 
 grid = [list(line) for line in open(0).read().splitlines()]
 
 R, C = len(grid), len(grid[0])
 
-G = nx.Graph()
+r, c = next((r, c) for r in range(R) for c in range(C) if grid[r][c] == "S")
 
-er, ec = next((r, c) for r in range(R) for c in range(C) if grid[r][c] == "E")
+dists = [[-1] * C for _ in range(R)]
+dists[r][c] = 0
+
+q = deque([(r, c)])
+
+while q:
+    cr, cc = q.popleft()
+    for nr, nc in [(cr + 1, cc), (cr - 1, cc), (cr, cc + 1), (cr, cc - 1)]:
+        if (
+            not (0 <= nr < R and 0 <= nc < C)
+            or grid[nr][nc] == "#"
+            or dists[nr][nc] != -1
+        ):
+            continue
+        dists[nr][nc] = dists[cr][cc] + 1
+        q.append((nr, nc))
+
+count = 0
 
 for r in range(R):
     for c in range(C):
-        if grid[r][c] in ".SE":
-            G.add_node((r, c))
+        if grid[r][c] == "#":
+            continue
 
-for r in range(R):
-    for c in range(C):
-        if grid[r][c] in ".SE":
-            for dr, dc in [(0, 1), (1, 0)]:
-                nr, nc = r + dr, c + dc
-                if 0 <= nr < R and 0 <= nc < C and grid[nr][nc] != "#":
-                    G.add_edge((r, c), (nr, nc))
+        for radius in range(2, 21):
+            for dr in range(radius + 1):
+                dc = radius - dr
+                for nr, nc in {
+                    (r + dr, c + dc),
+                    (r + dr, c - dc),
+                    (r - dr, c + dc),
+                    (r - dr, c - dc),
+                }:
+                    if not (0 <= nr < R and 0 <= nc < C):
+                        continue
+                    if grid[nr][nc] == "#":
+                        continue
+                    if dists[r][c] - dists[nr][nc] >= 100 + radius:
+                        count += 1
 
-base = dict(nx.shortest_path_length(G, (er, ec)))
-
-
-def get_reachable(r: int, c: int, max_dist: int) -> list[tuple[int, int]]:
-    reachable = []
-    for dr in range(-max_dist, max_dist + 1):
-        remaining = max_dist - abs(dr)
-        for dc in range(-remaining, remaining + 1):
-            if dr == dc == 0:
-                continue
-            nr, nc = r + dr, c + dc
-            if 0 <= nr < R and 0 <= nc < C and grid[nr][nc] != "#":
-                reachable.append((nr, nc))
-    return reachable
-
-
-def solve(cheat_size: int) -> int:
-    t = 0
-    for node in G.nodes:
-        r, c = node
-        reachable = get_reachable(r, c, cheat_size)
-        for dr, dc in reachable:
-            cost = abs(r - dr) + abs(c - dc)
-            saving = base[(r, c)] - (base[(dr, dc)] + cost)
-            if saving >= 100:
-                t += 1
-    return t
-
-
-print(solve(20))
+print(count)
