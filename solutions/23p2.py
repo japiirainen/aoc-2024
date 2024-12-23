6e7feef7084badd2d5abcbbba4c0ccedbfe79bbe
japiirainen <joona.piirainen@gmail.com>
2024-12-23 07:53:43 +0200

    feat(23): part 2 without networkx

--- solutions/23p2.py
+++ solutions/23p2.py
@@ -1,20 +1,31 @@
 #!/usr/bin/env python
 
-import sys
-import networkx as nx
+from collections import defaultdict
 
-G = nx.Graph()
+conns = defaultdict(set)
 
 for line in open(0).read().splitlines():
     a, b = line.split("-")
-    G.add_edge(a, b)
+    conns[a].add(b)
+    conns[b].add(a)
 
-best = set()
+sets = set()
 
-for i in range(2, sys.maxsize):
-    clique = next(nx.community.k_clique_communities(G, i), set())
-    if not clique:
-        break
-    best = clique
 
-print(*list(sorted(best)), sep=",")
+def search(node: str, req: set[str]):
+    k = tuple(sorted(req))
+    if k in sets:
+        return
+    sets.add(k)
+    for n in conns[node]:
+        if n in req:
+            continue
+        if not all(n in conns[q] for q in req):
+            continue
+        search(n, req | {n})
+
+
+for a in conns:
+    search(a, {a})
+
+print(*list(sorted(max(sets, key=len))), sep=",")
