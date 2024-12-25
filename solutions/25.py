c961c06fc262563c97914b1a7cd55879ee152a24
japiirainen <joona.piirainen@gmail.com>
2024-12-25 08:42:19 +0200

    feat(25): add solution

---
+++ solutions/25.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from itertools import takewhile
+
+lhs, khs = [], []
+
+T = lambda xs: list(zip(*xs))
+H = lambda xs: [len(list(takewhile(lambda c: c == "#", x))) - 1 for x in xs]
+
+for block in open(0).read().split("\n\n"):
+    block = block.splitlines()
+    if all(x == "#" for x in block[0]):
+        lhs.append(H(T(block)))
+    else:
+        khs.append(H([x[::-1] for x in T(block)]))
+
+print(sum(all(a + b < 6 for a, b in zip(lh, kh)) for lh in lhs for kh in khs))
